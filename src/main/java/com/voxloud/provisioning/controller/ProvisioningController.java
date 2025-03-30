38e3f26556d5b5b18b4164bed14aba6a7e93a30e
n0tx <rcandra91@msn.com>
2025-03-30 15:21:28 +0700

    Add ProvisioningController to handle HTTP GET requests for device provisioning

--- src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
+++ src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
@@ -1,5 +1,11 @@
 package com.voxloud.provisioning.controller;
 
+import com.voxloud.provisioning.service.ProvisioningService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class ProvisioningController {
 
-    // TODO Implement controller method
+    @Autowired
+    private ProvisioningService provisioningService;
+
+    @GetMapping("/provisioning/{macAddress}")
+    public ResponseEntity<String> getProvisioning(@PathVariable String macAddress) {
+        String provisioningFile = provisioningService.getProvisioningFile(macAddress);
+        if (provisioningFile == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(" Device not found ");
+        }
+        return ResponseEntity.ok(provisioningFile);
+    }
 }
